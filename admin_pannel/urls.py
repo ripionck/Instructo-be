608b3f97cc63f32fa997456d0271c98a50abfd66
ripionck <ripion.chakmar@gmail.com>
2024-12-14 04:27:28 +0600

    added admin pannel urls

---
+++ admin_pannel/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    AdminApiView,
+    AdminLoginApiView,
+    AdminLogoutApiView,
+    AdminRegistrationView,
+    activate_view
+)
+
+urlpatterns = [
+    path('list/', AdminApiView.as_view(), name='admin-list'),
+    path('register/', AdminRegistrationView.as_view(), name='register'),
+    path('login/', AdminLoginApiView.as_view(), name='login'),
+    path('logout/', AdminLogoutApiView.as_view(), name='logout'),
+    path('active/<str:uid64>/<str:token>/', activate_view, name='activate'),
+]
