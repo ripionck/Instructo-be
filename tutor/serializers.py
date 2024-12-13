5cd63090fdb805306907bb6a233ba18ebf341e0b
ripionck <ripion.chakmar@gmail.com>
2024-12-14 05:24:12 +0600

    updated tutor serializers

--- tutor/serializers.py
+++ tutor/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Tutor, Review
+from .models import Tutor, Review, SubjectChoice
+
+
+class SubjectChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectChoice
+        fields = '__all__'
 
 
 class TutorSerializer(serializers.ModelSerializer):
+    subjects = SubjectChoiceSerializer(many=True, read_only=True)
+
     class Meta:
         model = Tutor
         fields = '__all__'
