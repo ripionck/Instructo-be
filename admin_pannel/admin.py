4895cb359e279c5fe21653f8fb391b92ace5304f
ripionck <ripion.chakmar@gmail.com>
2024-12-14 04:24:05 +0600

    register admin model and added views

--- admin_pannel/admin.py
+++ admin_pannel/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AdminModel
 
-# Register your models here.
+
+@admin.register(AdminModel)
+class AdminModelAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'mobile_no']
+    search_fields = ['user__first_name', 'user__last_name', 'mobile_no']
+    list_filter = ['user__date_joined']
+
+    def first_name(self, obj):
+        return obj.user.first_name
+    first_name.admin_order_field = 'user__first_name'
+
+    def last_name(self, obj):
+        return obj.user.last_name
+    last_name.admin_order_field = 'user__last_name'
