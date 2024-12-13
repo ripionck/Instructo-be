2a46e16ecb8a04b6598c356cd7f324f7f746f353
ripionck <ripion.chakmar@gmail.com>
2024-12-14 05:21:06 +0600

    registered tutor model to admin

--- tutor/admin.py
+++ tutor/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Tutor, Review, SubjectChoice
 
-# Register your models here.
+
+class TutorModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'tuition_class',
+        'availability',
+        'medium',
+        'student_gender',
+        'tutor_gender',
+        'number_of_students',
+        'tutoring_experience',
+        'salary',
+        'location'
+    )
+    list_filter = ('tuition_class', 'availability', 'medium')
+    search_fields = ('title', 'description', 'location')
+
+
+admin.site.register(Tutor, TutorModelAdmin)
+admin.site.register(Review)
+admin.site.register(SubjectChoice)
