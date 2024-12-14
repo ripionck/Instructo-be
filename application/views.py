9727d2be89fdee6e84acf3d9041c8ce8a4d9ec0b
ripionck <ripion.chakmar@gmail.com>
2024-12-14 06:17:20 +0600

    updated application views

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Application
-from .serializer import ApplicationSerializer
+from .serializers import ApplicationSerializer
 
 
 class ApplicationListCreateView(APIView):
